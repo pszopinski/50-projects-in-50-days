1baccc250ca2c76d9068ac9d50060dfb24014464
Paweł Szopiński <contact@pawelszopinski.com>
2024-04-05 02:54:23 +0200

    Add functionality

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -1,86 +1,90 @@
-// const textarea = document.querySelector("textarea");
-// const choiceList = document.querySelector(".choice-list");
-// const results = document.querySelector(".results");
-// const resetBtn = results.querySelector("button");
+const textarea = document.querySelector("textarea");
+const choiceList = document.querySelector(".choice-list");
+const results = document.querySelector(".results");
+const resetBtn = results.querySelector("button");
 
-// textarea.addEventListener("keyup", updateChoices);
+textarea.addEventListener("keyup", updateChoices);
 
-// textarea.addEventListener("keydown", (e) => {
-//   if (e.key == "Enter") {
-//     e.preventDefault();
-//     choose();
-//   }
-// });
+textarea.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+    textarea.value ? choose() : textarea.reportValidity();
+  }
+});
 
-// textarea.addEventListener("click", () => {
-//   console.log(textarea.readOnly);
-//   if (textarea.readOnly) {
-//     reset();
-//   }
-// });
+textarea.addEventListener("click", () => {
+  if (textarea.readOnly) {
+    resetBtn.focus();
+  }
+});
 
-// resetBtn.addEventListener("click", reset);
+resetBtn.addEventListener("click", reset);
 
-// // There might already be some content when the page loads
-// if (textarea.value) {
-//   updateChoices();
-// }
+// There might already be some content when the page loads
+if (textarea.value) {
+  updateChoices();
+}
 
-// function updateChoices() {
-//   choiceList.innerHTML = textarea.value
-//     .split(",")
-//     .filter((choice) => choice.trim())
-//     .map((choice) => `<li class="tag">${choice}</li>`)
-//     .join("");
-// }
+function updateChoices() {
+  choiceList.innerHTML = textarea.value
+    .split(",")
+    .filter((choice) => choice.trim())
+    .map((choice) => `<li class="tag">${choice}</li>`)
+    .join("");
+}
 
-// function choose() {
-//   textarea.readOnly = true;
+function choose() {
+  textarea.readOnly = true;
 
-//   const ourChoiceIdx = Math.floor(Math.random() * choiceList.childElementCount);
-//   const duration = spinTheWheel(ourChoiceIdx);
+  const ourChoiceIdx = Math.floor(Math.random() * choiceList.childElementCount);
+  const duration = spinTheWheel(ourChoiceIdx);
 
-//   setTimeout(() => {
-//     resetBtn.focus();
-//     results.classList.remove("hide");
-//   }, duration + 200);
-// }
+  setTimeout(() => {
+    resetBtn.focus();
+    results.classList.remove("hide");
+  }, duration + 200);
+}
 
-// function spinTheWheel(ourChoiceIdx) {
-//   const choices = choiceList.children;
+function spinTheWheel(ourChoiceIdx) {
+  const choices = choiceList.children;
 
-//   // Select every element in a spinning wheel fasion
-//   const iterationCount =
-//     Math.floor(20 / choices.length) * choices.length + ourChoiceIdx;
-//   let delay = 200;
-//   let duration = 100;
-//   for (let i = 0; i < iterationCount; ++i) {
-//     selectChoiceTemporarily(i % choices.length, duration, delay);
-//     delay += duration;
-//     duration += 10; // spinning wheel slows down
-//   }
+  // Select every element in a spinning wheel fashion
+  const iterationCount =
+    Math.floor(20 / choices.length) * choices.length + ourChoiceIdx;
+  let delay = 200;
+  let duration = 100;
+  for (let i = 0; i < iterationCount; ++i) {
+    selectChoiceTemporarily(i % choices.length, duration, delay);
+    delay += duration;
+    duration += 10; // spinning wheel slows down
+  }
 
-//   setTimeout(() => {
-//     const ourChoice = choices[ourChoiceIdx];
-//     ourChoice.classList.add("selected");
-//     chosenTag.textContent = ourChoice.textContent;
-//     results.classList.remove("hide");
-//   }, delay);
+  setTimeout(() => {
+    const ourChoice = choices[ourChoiceIdx];
+    ourChoice.classList.add("selected");
+    chosenTag.textContent = ourChoice.textContent;
+    results.classList.remove("hide");
+  }, delay);
 
-//   return delay;
-// }
+  return delay;
+}
 
-// function selectChoiceTemporarily(idx, duration, delay) {
-//   const choice = choiceList.children[idx];
-//   setTimeout(() => {
-//     choice.classList.add("selected");
-//   }, delay);
-//   setTimeout(() => choice.classList.remove("selected"), delay + duration);
-// }
+function selectChoiceTemporarily(idx, duration, delay) {
+  setTimeout(() => {
+    const choice = choiceList.children[idx];
+    choice.classList.add("selected");
+  }, delay);
 
-// function reset() {
-//   results.classList.add("hide");
-//   updateChoices();
-//   textarea.readOnly = false;
-//   textarea.select();
-// }
+  setTimeout(() => {
+    const choice = choiceList.children[idx];
+    choice.classList.remove("selected");
+  }, delay + duration);
+}
+
+function reset() {
+  chosenTag.textContent = "";
+  results.classList.add("hide");
+  updateChoices();
+  textarea.readOnly = false;
+  textarea.select();
+}
