dfe167eb4b8fcd96257373a80eb7c8e357a5676c
Paweł Szopiński <contact@pawelszopinski.com>
2024-05-23 00:09:10 +0200

    [day-19] Update the time
    Update time display every second.
    Rotate the hands in a naive way.

--- theme_clock/script.js
+++ theme_clock/script.js
@@ -1,4 +1,9 @@
 const toggleDarkModeBtn = document.getElementById("toggleDarkMode");
+const hourHand = document.getElementById("hourHand");
+const minuteHand = document.getElementById("minuteHand");
+const secondHand = document.getElementById("secondHand");
+const timeDisplay = document.getElementById("time");
+const dateDisplay = document.getElementById("date");
 
 let darkMode = false;
 function toggleDarkMode(force) {
@@ -6,7 +11,37 @@ function toggleDarkMode(force) {
   toggleDarkModeBtn.textContent = darkMode ? "Dark mode" : "Light mode";
 }
 
-toggleDarkMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
+function updateTime() {
+  const now = new Date();
+  updateClockHands(now);
+  updateDisplay(now);
+}
+
+function updateClockHands(date) {
+  const seconds = date.getSeconds();
+  const minutes = date.getMinutes() + seconds / 60;
+  const hours = date.getHours() + minutes / 60;
+
+  hourHand.style.rotate = `${hours / 12}turn`;
+  minuteHand.style.rotate = `${minutes / 60}turn`;
+  secondHand.style.rotate = `${seconds / 60}turn`;
+}
+
+function updateDisplay(date) {
+  const timeStr = Intl.DateTimeFormat(undefined, {
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(date);
+  const dateStr = Intl.DateTimeFormat(undefined, {
+    weekday: "long",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  }).format(date);
+
+  timeDisplay.textContent = timeStr;
+  dateDisplay.textContent = dateStr;
+}
 
 window
   .matchMedia("(prefers-color-scheme: dark)")
@@ -16,3 +51,10 @@ window
 toggleDarkModeBtn.addEventListener("click", () => {
   toggleDarkMode();
 });
+
+setInterval(() => {
+  updateTime();
+}, 1000);
+
+toggleDarkMode(window.matchMedia("(prefers-color-scheme: dark)").matches);
+updateTime();
