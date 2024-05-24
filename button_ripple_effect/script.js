6c340c68d8f0679da47eed8122a2cf0286d2f9de
Paweł Szopiński <contact@pawelszopinski.com>
2024-05-24 13:27:24 +0200

    [day-20] Implement ripple animation
    Use @property and CSS transitions

---
+++ button_ripple_effect/script.js
@@ -0,0 +1,52 @@
+class RipplePosition {
+  constructor() {
+    window.CSS.registerProperty({
+      name: "--x",
+      syntax: "<length-percentage>",
+      inherits: true,
+      initialValue: "50%",
+    });
+
+    window.CSS.registerProperty({
+      name: "--y",
+      syntax: "<length-percentage>",
+      inherits: true,
+      initialValue: "50%",
+    });
+  }
+
+  get x() {
+    return document.documentElement.style.getPropertyValue("--x");
+  }
+
+  get y() {
+    return document.documentElement.style.getPropertyValue("--y");
+  }
+
+  set x(value) {
+    return document.documentElement.style.setProperty("--x", value);
+  }
+
+  set y(value) {
+    return document.documentElement.style.setProperty("--y", value);
+  }
+}
+
+const btn = document.getElementById("btn");
+btn.showRipple = function (clickEvent) {
+  if (this.classList.contains("clicked")) {
+    return;
+  }
+
+  const boundingRect = this.getBoundingClientRect();
+  this.ripplePosition.x = `${clickEvent.x - boundingRect.x}px`;
+  this.ripplePosition.y = `${clickEvent.y - boundingRect.y}px`;
+
+  this.classList.add("clicked");
+  setTimeout(() => {
+    this.classList.remove("clicked");
+  }, 1000);
+};
+
+btn.ripplePosition = new RipplePosition();
+btn.addEventListener("click", btn.showRipple);
